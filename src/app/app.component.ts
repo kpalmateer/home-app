23b8c9e393f74bd5e48f3b7a1d4347b1ed499d40
kpalmateer <98238056+kpalmateer@users.noreply.github.com>
2024-04-29 22:46:19 -0500

    Build basic resume page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+// import new components here after running ng generate component COMP_NAME
+import { ResumeComponent } from './resume/resume.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ResumeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
