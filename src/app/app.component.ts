4328d2e941d63751e4ab9ffcdbb7ecabc46d075d
kpalmateer <98238056+kpalmateer@users.noreply.github.com>
2024-05-01 00:33:57 -0500

    Add nav bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 // import new components here after running ng generate component COMP_NAME
 import { ResumeComponent } from './resume/resume.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ResumeComponent],
+  imports: [RouterOutlet, ResumeComponent, NavBarComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ResumeComponent } from './resume/resume.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'resume',
+        component: ResumeComponent,
+        title: 'Resume'
+    }
+];
+
+export default routes;
